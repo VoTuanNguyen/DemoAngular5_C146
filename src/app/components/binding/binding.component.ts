34c42af661863bce3c497922dac571437096ea75
Vo Tuan Nguyen (FA.G0) <NguyenVT2@fsoft.com.vn>
2018-10-12 08:39:22 +0700

    no hope

--- src/app/components/binding/binding.component.ts
+++ src/app/components/binding/binding.component.ts
@@ -5,18 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './binding.component.html',
   styleUrls: ['./binding.component.css']
 })
+
 export class BindingComponent implements OnInit {
   en: string = "Hello World!";
   vi: string = "Chào thế giới!";
   flag: boolean = false;
   myColor: string = 'red';
   textInput: string = 'Xin chào các bạn!';
-  color: boolean = false;
+  color: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+    this.isShow = false;
   }
+
   changeColor() {
     this.color = !this.color;
     this.myColor = this.color ? 'red' : 'lightblue';
